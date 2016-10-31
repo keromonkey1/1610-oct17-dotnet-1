96380d7a41c757e49f6c9b2f7dd0c8a3966e3dac
fred belotte <fred@revature.com>
2016-10-31 12:03:44 -0400

    add the servic eproject.

---
+++ MonsterApp/MonsterApp.DataClient/IMonsterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MonsterApp.DataClient
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  [ServiceContract]
+  public interface IMonsterService
+  {
+    [OperationContract]
+    string DoWork();
+  }
+}
